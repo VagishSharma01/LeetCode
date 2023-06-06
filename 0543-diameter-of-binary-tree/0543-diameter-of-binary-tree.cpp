9bfbb1663505e3760ce688a17785ecda55539f51
VagishSharma01 <108809125+VagishSharma01@users.noreply.github.com>
2023-06-06 19:11:44 +0530

    Time: 13 ms (52.67%), Space: 20.6 MB (10.25%) - LeetHub

--- 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.cpp
+++ 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.cpp
@@ -11,33 +11,69 @@
  */
 class Solution {
 public:
-     int height(TreeNode* root)
-{
-    //Base Case
-    if(root==NULL)
-    {
-        return 0;
-    }
-    int left = height(root->left);
-    int right = height(root->right);
+//      int height(TreeNode* root)
+// {
+//     //Base Case
+//     if(root==NULL)
+//     {
+//         return 0;
+//     }
+//     int left = height(root->left);
+//     int right = height(root->right);
    
     
-    int ans = max(left,right) +1;
+//     int ans = max(left,right) +1;
     
-    return ans;
-}
+//     return ans;
+// }
     
-    int diameterOfBinaryTree(TreeNode* root) {
+//     int diameterOfBinaryTree(TreeNode* root) {
+//        if(root==NULL)
+//         {
+//             return 0;
+//         }
+        
+//         int left_d = diameterOfBinaryTree(root->left);
+//         int right_d = diameterOfBinaryTree(root->right);
+//         int left_right_d = height(root->left) + height(root->right) ;
+        
+//         int ans = max(left_d,max(right_d,left_right_d));
+//         return ans;
+//     }
+    
+    
+     pair<int,int> diameterFast(TreeNode* root) {
+         
        if(root==NULL)
         {
-            return 0;
+            pair<int,int> p;
+           p.first =0;
+           p.second=0;
+           return p;
         }
+         
+         pair<int,int> left = diameterFast(root->left);
+         pair<int,int> right = diameterFast(root->right);
+         
+         int l_height = left.first;
+         int r_height = right.first;
+         int l_dia = left.second;
+         int r_dia = right.second;
+         
+         int height = max(l_height,r_height)+1;
+         int diameter = max(l_height+r_height,max(l_dia,r_dia));
+         
+         pair<int,int> ans;
+         ans.first = height;
+         ans.second = diameter;
         
-        int left_d = diameterOfBinaryTree(root->left);
-        int right_d = diameterOfBinaryTree(root->right);
-        int left_right_d = height(root->left) + height(root->right) ;
-        
-        int ans = max(left_d,max(right_d,left_right_d));
-        return ans;
+         return ans;
     }
+    
+    int diameterOfBinaryTree(TreeNode* root){
+        pair<int,int> ans = diameterFast(root);
+        return ans.second;
+    }
+    
+    
 };
