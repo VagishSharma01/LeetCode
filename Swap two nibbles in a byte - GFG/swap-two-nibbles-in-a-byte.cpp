52ff916d4dad7cd4f83a2602ba75c533889f4c15
VagishSharma01 <108809125+VagishSharma01@users.noreply.github.com>
2023-06-07 17:06:47 +0530

    Added solution - LeetHub

---
+++ Swap two nibbles in a byte - GFG/swap-two-nibbles-in-a-byte.cpp
@@ -0,0 +1,28 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h> 
+using namespace std;
+
+// } Driver Code Ends
+class Solution{
+public:
+    int swapNibbles(int x){
+        // code here
+        return ( (x & 0x0F) << 4 | (x & 0xF0) >> 4 );
+    }
+};
+
+//{ Driver Code Starts.
+int main() 
+{ 
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int N;
+        cin>>N;
+        Solution ob;
+        cout << ob.swapNibbles(N) << endl;
+    }
+    return 0; 
+}
+// } Driver Code Ends
