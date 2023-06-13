e9c6fce0243beec70eebd77aeb755deff993bbbf
VagishSharma01 <108809125+VagishSharma01@users.noreply.github.com>
2023-06-14 00:12:18 +0530

    Time: 17 ms (58.94%), Space: 10.7 MB (50.72%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,37 +1,57 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
+        
+        unordered_map<int,int> mp;
         vector<int> ans;
-       vector<pair<int,int>> p;
-        int n = nums.size();
         
-        for(int i=0;i<n;i++)
+        for(int i=0;i<nums.size();i++)
         {
-            p.push_back({nums[i],i});
+            int f = target-nums[i];
+            if(mp.find(f) != mp.end())
+            {
+                ans.push_back(mp[f]);
+                ans.push_back(i);
+                 return ans;
+            }
+            
+          mp[nums[i]] = i; 
+            
+            
         }
-        sort(p.begin(),p.end());
         
-        int left=0;
-        int right=nums.size()-1;
-        while(left<right)
-        {
-            int sum =p[left].first+p[right].first;
-            if(target==sum)
-            {
-                ans.push_back(p[left].second);
-                ans.push_back(p[right].second);
-                return ans;
-            }
-            else if(sum>target)
-            {
-                right--;
-            }
-            else
-            {
-                left++;
-            }
-        }
-       
         return ans;
+//         vector<int> ans;
+//        vector<pair<int,int>> p;
+//         int n = nums.size();
+        
+//         for(int i=0;i<n;i++)
+//         {
+//             p.push_back({nums[i],i});
+//         }
+//         sort(p.begin(),p.end());
+        
+//         int left=0;
+//         int right=nums.size()-1;
+//         while(left<right)
+//         {
+//             int sum =p[left].first+p[right].first;
+//             if(target==sum)
+//             {
+//                 ans.push_back(p[left].second);
+//                 ans.push_back(p[right].second);
+//                 return ans;
+//             }
+//             else if(sum>target)
+//             {
+//                 right--;
+//             }
+//             else
+//             {
+//                 left++;
+//             }
+//         }
+       
+//         return ans;
     }
 };
