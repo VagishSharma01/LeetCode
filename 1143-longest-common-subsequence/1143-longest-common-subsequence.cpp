2456ed2c579c3ebf74335fa6524b8362f4d4af44
VagishSharma01 <108809125+VagishSharma01@users.noreply.github.com>
2023-06-24 01:22:51 +0530

    Time: 98 ms (26.16%), Space: 24.5 MB (46.24%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -1,29 +1,57 @@
 class Solution {
 public:
-    vector<vector<int>> dp;
-    int f(const string& t1,const string& t2,int ind1,int ind2 )
-    {
-        if(ind1==-1 || ind2==-1)
-        {
-            return 0;
-        }
-        if(dp[ind1][ind2]!=-1)
-        {
-            return dp[ind1][ind2];
-        }
+    
+//     int f(string &t1,string &t2,int ind1,int ind2 )
+//     {
+//         if(ind1==0 || ind2==0)
+//         {
+//             return 0;
+//         }
+//         if(dp[ind1][ind2]!=-1)
+//         {
+//             return dp[ind1][ind2];
+//         }
         
-        if(t1[ind1]==t2[ind2])
-        {
-            return dp[ind1][ind2]= 1 + f(t1,t2,ind1-1,ind2-1);
-        }
-        return  dp[ind1][ind2]= max(f(t1,t2,ind1-1,ind2),f(t1,t2,ind1,ind2-1));
-    }
+//         if(t1[ind1-1]==t2[ind2-1])
+//         {
+//             return dp[ind1][ind2]= 1 + f(t1,t2,ind1-1,ind2-1);
+//         }
+//         return  dp[ind1][ind2]= max(f(t1,t2,ind1-1,ind2),f(t1,t2,ind1,ind2-1));
+//     }
+    
+    
     
     int longestCommonSubsequence(string text1, string text2) {
         int ind1 = text1.length();
         int ind2 = text2.length();
-        dp.resize(ind1, vector<int>(ind2, -1));
-       return  f(text1,text2,ind1-1,ind2-1);
+        
+       // return  f(text1,text2,ind1,ind2);
+        vector<vector<int>> dp(ind1+1,vector<int>(ind2+1,0)) ;
+        for(int i=0;i<ind1;i++)
+        {
+            dp[i][0]=0;
+        }
+        for(int j=0;j<ind2;j++)
+        {
+            dp[0][j]=0;
+        }
+        for(int i=1;i<=ind1;i++)
+        {
+            for(int j=1;j<=ind2;j++)
+            {
+                if(text1[i-1]==text2[j-1])
+                {
+                 dp[i][j]= 1 + dp[i-1][j-1];
+                }
+                 else
+                 {
+                     dp[i][j]= max(dp[i-1][j],dp[i][j-1]);
+                 }
+            }
+        }
+        
+        return dp[ind1][ind2];
+        
     }
 };
 // class Solution {
