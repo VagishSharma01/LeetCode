4293ca26769fc3cf2dbd6279d6b26d6efb46b7ed
VagishSharma01 <108809125+VagishSharma01@users.noreply.github.com>
2023-08-13 04:53:07 +0530

    Time: 8 ms (32.23%), Space: 7.6 MB (31.26%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -1,17 +1,29 @@
 class Solution {
 public:
+    
+    bool isvalid(char ch)
+    {
+        if( (ch>='a' && ch<= 'z') || (ch>='A' && ch<= 'Z') || (ch>='0' && ch<= '9'))
+        {
+            return 1;
+        }
+        return 0;
+    }
+    
     bool isPalindrome(string s) {
         string helper = "";
-        for (char c : s) {
-            if (isalnum(c)) {
-                helper += tolower(c);
+        for(int i=0;i<s.size();i++)
+        {
+            if(isvalid(s[i]))
+            {
+                helper.push_back(s[i]);
             }
         }
         
         int i = 0;
         int j = helper.length() - 1;
         while (i < j) {
-            if (helper[i] != helper[j]) {
+            if (tolower(helper[i]) != tolower(helper[j])) {
                 return false;
             }
             i++;
