aa9c035cd81bde1d5bbef1421fc8a14fcf622d1d
VagishSharma01 <108809125+VagishSharma01@users.noreply.github.com>
2023-08-30 03:06:09 +0530

    Time: 236 ms (65.34%), Space: 237.9 MB (5.05%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -1,54 +1,37 @@
-
 class Solution {
 public:
     
-    bool solve(int ind,int target,vector<int>&arr, vector<vector<int>> &dp)
+    bool solve(int ind,int target,vector<int> &arr,vector<vector<int>> &dp)
     {
-        //base
-        if(target==0)
-        {
-            return true;
-        }
-        if(ind==0)
-        {
-            return (arr[ind]==target);
-        }
-        if(dp[ind][target] != -1)
-        {
-            return dp[ind][target];
-        }
-        
+        if(ind==0) return (arr[ind]==target);
+        if(target==0) return true;
+        if(dp[ind][target]!=-1) return dp[ind][target];
         bool not_take = solve(ind-1,target,arr,dp);
         bool take = false;
-        if(target>=arr[ind])
+        if(arr[ind]<=target)
         {
-            take = solve(ind-1,target-arr[ind],arr,dp);
+            take =  solve(ind-1,target-arr[ind],arr,dp);
         }
-        
-        return dp[ind][target] = take || not_take;
+        return dp[ind][target] = take||not_take;
         
     }
     
     bool canPartition(vector<int>& nums) {
+        int sum = 0;
         int n = nums.size();
-        int sum=0;
-        for(int i=0;i<n;i++)
+        for(int i=0;i<nums.size();i++)
         {
-         sum = sum+nums[i];   
+            sum += nums[i];
         }
-        
-        int target = sum/2;
-        
-        vector<vector<int>> dp(n+1,vector<int>(sum+1,-1));
-        
-        if(sum%2 != 0)
+        int target=sum/2;
+        vector<vector<int>> dp(nums.size()+1,vector<int> (sum+1,-1));
+        if(sum%2 == 0)
         {
-            return false;
+            return solve(n-1,target,nums,dp);
         }
         else
         {
-            bool ans = solve(n-1,target,nums,dp);
-            return ans;
+            return false;
         }
     }
 };
