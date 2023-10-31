2ae07b9ef56558154fb35e9997e55b64dafc3ef9
VagishSharma01 <108809125+VagishSharma01@users.noreply.github.com>
2023-10-31 18:39:10 +0530

    Added solution - LeetHub

---
+++ Find the number of islands - GFG/find-the-number-of-islands.cpp
@@ -0,0 +1,84 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+  public:
+  
+  void bfs(int r,int c,vector<vector<char>>& grid,vector<vector<int>>& vis)
+    {
+        int n = grid.size();
+        int m = grid[0].size();
+
+        vis[r][c]=1;
+        queue<pair<int,int>> q;
+        q.push({r,c});
+        while(!q.empty())
+        {
+            int row = q.front().first;
+            int col = q.front().second;
+            q.pop();
+            //Traverse the neighbours and mark them if its land and is not visited
+            for(int delrow=-1;delrow<=1;delrow++)
+            {
+                for(int delcol=-1;delcol<=1;delcol++)
+                {
+                    int nrow = row+delrow;
+                    int ncol = col+delcol;
+                    //has to be valid
+                    if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && grid[nrow][ncol] == '1' && !vis[nrow][ncol])
+                     {
+                         q.push({nrow,ncol});
+                         vis[nrow][ncol]=1;
+                     }
+                }
+            }
+        }
+    }
+    // Function to find the number of islands.
+    int numIslands(vector<vector<char>>& grid) {
+        // Code here
+        int n = grid.size();
+        int m = grid[0].size();
+        int cnt=0;
+        //create vis 
+        vector<vector<int>> vis(n,vector<int>(m,0));
+        //Traverse the visited array and call bfs to connect
+        // land and increase count as we find land
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<m;j++)
+            {
+                if(!vis[i][j] && grid[i][j]=='1')
+                {
+                    cnt++;
+                    bfs(i,j,grid,vis);
+                    
+                }
+            }
+        }
+        return cnt;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int tc;
+    cin >> tc;
+    while (tc--) {
+        int n, m;
+        cin >> n >> m;
+        vector<vector<char>> grid(n, vector<char>(m, '#'));
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                cin >> grid[i][j];
+            }
+        }
+        Solution obj;
+        int ans = obj.numIslands(grid);
+        cout << ans << '\n';
+    }
+    return 0;
+}
+// } Driver Code Ends
