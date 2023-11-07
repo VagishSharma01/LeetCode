6f3b748f345abf51ceed4e8577d42fb83b56d206
VagishSharma01 <108809125+VagishSharma01@users.noreply.github.com>
2023-11-07 10:17:09 +0530

    Added solution - LeetHub

--- Number of Distinct Islands - GFG/number-of-distinct-islands.cpp
+++ Number of Distinct Islands - GFG/number-of-distinct-islands.cpp
@@ -9,25 +9,26 @@ using namespace std;
 
 class Solution {
   private:
-   void dfs(int row,int col,vector<vector<int>> &grid,vector<vector<int>> &vis,vector<pair<int,int>> &p
-   ,int row0,int col0)
+   void dfs(int row,int col,vector<vector<int>> &grid,vector<vector<int>> &vis,vector<pair<int,int>> &p,
+   int row0,int col0)
    {
-       vis[row][col]=1;
-       int n=grid.size();
-       int m=grid[0].size();
-       p.push_back({row-row0,col-col0});
-       int drow[]= {-1,0,1,0};
-       int dcol[]= {0,1,0,-1};
-       for(int i=0;i<4;i++)
-       {
-           int nrow=row+drow[i];
-           int ncol=col+dcol[i];
-           if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && grid[nrow][ncol]==1 && vis[nrow][ncol]==0)
-           {
-               dfs(nrow,ncol,grid,vis,p,row0,col0);
-           }
-       }
-       
+       int n = grid.size();
+        int m = grid[0].size();
+        vis[row][col]=1;
+        p.push_back({row-row0,col-col0});
+        
+        int drow[]={-1,0,1,0};
+        int dcol[]={0,1,0,-1};
+        for(int i=0;i<4;i++)
+        {
+            int nrow=row+drow[i];
+            int ncol=col+dcol[i];
+            if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && grid[nrow][ncol]==1 && vis[nrow][ncol]==0)
+            {
+                dfs(nrow,ncol,grid,vis,p,row0,col0);
+            }
+        }
+        
    }
   public:
     int countDistinctIslands(vector<vector<int>>& grid) {
@@ -36,20 +37,23 @@ class Solution {
         int m = grid[0].size();
         vector<vector<int>> vis(n,vector<int>(m,0));
         set<vector<pair<int,int>>> s;
+        
+        //Go through every node and run dfs
         for(int i=0;i<n;i++)
         {
             for(int j=0;j<m;j++)
             {
                 if(grid[i][j]==1 && vis[i][j]==0)
                 {
-                vector<pair<int,int>> p;
-                dfs(i,j,grid,vis,p,i,j);
-                s.insert(p);
+                    vector<pair<int,int>> p;
+                    dfs(i,j,grid,vis,p,i,j);
+                    s.insert(p);
                 }
             }
         }
         return s.size();
     }
+    
 };
 
 
